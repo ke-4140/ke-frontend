c68a7020895877b19abdb85cfaa96f394bd8a5be
Taryn Tsui <taryntny@gmail.com>
2021-03-23 03:11:11 +0800

    progressbar

--- src/features/editor/Editor.js
+++ src/features/editor/Editor.js
@@ -1,48 +1,64 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Header } from '../../components/Header';
-import { Card } from '../../components/Card';
-import { useDrag } from 'react-dnd';
-import { Timeline }from './Timeline'
-// import {
-//   decrement,
-//   increment,
-//   incrementByAmount,
-//   incrementAsync,
-//   selectCount,
-// } from './counterSlice';
-// import styles from './Counter.module.css';
+import { Timeline } from './Timeline'
+import YouTube from 'react-youtube';
+import StyledContentLoader from 'styled-content-loader'
 
 export function Editor() {
-  // const count = useSelector(selectCount);
-  // const dispatch = useDispatch();
-  // const [incrementAmount, setIncrementAmount] = useState('2');
 
-  // const [{ opacity }, dragRef] = useDrag(
-  //   () => ({
-  //     collect: (monitor) => ({
-  //       opacity: monitor.isDragging() ? 0.5 : 1
-  //     })
-  //   }),
-  //   []
-  // )
+  const [youtubeID, setYoutubeID] = useState("iJDoc0kvXLc");
+  const [playerObj, setPlayerObj] = useState(null);
+  const [seconds, setSeconds] = useState(800);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const opts = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const onReady = (e) => {
+    console.log(e.target);
+    setPlayerObj(e.target);
+    setSeconds(e.target.getDuration());
+    setIsLoading(false);
+    e.target.playVideo();
+  }
+
+  function onStateChange(e) {
+    console.log(e.target.getCurrentTime());
+  }
+
+  function onFrameSelect(second) {
+    console.log("seek to: ", second);
+    playerObj.seekTo(second);
+    playerObj.playVideo();
+  }
 
   return (
     <div>
       <Header />
 
-      <div style={{ display: 'flex', flexDirection: 'row', justifyContent:'space-between', height: '400px', margin: 20}}>
+      <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', height: '400px', margin: 20 }}>
         <div>
           <span>Instruction</span>
+          <ol>
+            <li> Add Keyframe by pressing a frame on Timeline </li>
+            <li> Remove keyframe by pressing a Keyframe on Timeline </li>
+            <li> Export when you feel good about all the Keyframes </li>
+          </ol>
         </div>
-        <div>
-          <iframe width="560px" height="340px"
-            src="https://www.youtube.com/embed/iJDoc0kvXLc?controls=0?enablejsapi=1">
-          </iframe>
-        </div>
+        <YouTube videoId={youtubeID} opts={opts} onReady={onReady} onStateChange={onStateChange} />
       </div>
 
-    <Timeline/>
+      {!isLoading ? (
+        <Timeline isLoading={isLoading} seconds={seconds} player={playerObj} onFrameSelect={onFrameSelect} />
+      ) : (
+        <></>
+      )}
 
     </div >
   );
--- src/features/editor/Timeline.js
+++ src/features/editor/Timeline.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Timeline.css';
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
@@ -8,13 +8,23 @@ const reorder = (list, startIndex, endIndex) => {
   return list;
 };
 
-export function Timeline() {
-  const [frames, setFrames] = useState(Array.from({ length: 200 }, (v, k) => k).map(k => ({
+export function Timeline({ seconds, player, onFrameSelect }) {
+  const [frames, setFrames] = useState(Array.from({ length: seconds }, (v, k) => k).map(k => ({
     id: `item-${k}`,
     value: "",
     isKey: false
   })));
-  const [width, changeWidth] = useState(20);
+  const [width, changeWidth] = useState(10);
+  const [statusText, setStatusText] = useState("");
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress(player.getCurrentTime());
+    }
+      , 1000);
+    return () => clearInterval(interval);
+  }, []);
 
   function onDragEnd(result) {
     if (!result.destination) {
@@ -33,17 +43,31 @@ export function Timeline() {
     setFrames(reorderedFrames);
   }
 
-  function markKeyFrame (index) {
+  function markKeyFrame(index) {
     // frames[frameId].isKey=!frames[frameId].isKey;
     const newFrames = [...frames];
-    newFrames[ index ].isKey = !newFrames[ index ].isKey;
+
+    newFrames[index].isKey = !newFrames[index].isKey;
     setFrames(newFrames);
+    onFrameSelect(index);
+    var helperText = newFrames[index].isKey ? "Added" : "Removed";
+    setStatusText(helperText + " frame at " + secondsToMinutes(index));
   }
+
+  function secondsToMinutes(s) {
+    var m = Math.floor(s / 60);
+    var s = s % 60;
+    if (s < 10) return `${m}:0${s}`
+    else return `${m}:${s}`;
+  }
+
   return (
     <div>
       <div class="controlPane">
-        <span>Timeline</span>
+        <span>Timeline </span>
+
         <div class="controlButtons">
+          <span> {statusText} /{secondsToMinutes(seconds)} </span>
           <button onClick={() => changeWidth(width + 10)}>
             +
           </button>
@@ -52,28 +76,34 @@ export function Timeline() {
           </button>
         </div>
       </div>
+
+
       <DragDropContext onDragEnd={onDragEnd}>
         <Droppable droppableId="drappable" direction="horizontal">
           {(provided) => (
             <div class="wrapper" ref={provided.innerRef} {...provided.droppableProps}>
+              <div class="progressBar">
+                <div class="progressNode" style={{ width: progress  }} />
+              </div>
               {frames.map((frame, index) =>
                 <Draggable draggableId={frame.id} index={index} key={frame.id}>
                   {(provided, snapshot) => {
                     var style = {
                       width: width,
-                      background: frame.isKey ? 'red' : 'beige',
+                      borderWidth: frame.isKey ? 1 : 0,
                       ...provided.draggableProps.style,
                     }
                     return (
                       <div
                         class="item"
                         ref={provided.innerRef}
-                        onClick={()=>markKeyFrame(index)}
+                        onClick={() => markKeyFrame(index)}
+                        disabled={frame.isKey ? false : true}
                         {...provided.draggableProps}
                         {...provided.dragHandleProps}
                         style={style}
                       >
-                        {frame.isKey? '':''}
+                        {frame.isKey ? '-' : ''}
                       </div>
                     );
                   }}
